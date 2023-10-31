328dbd38ac9a9ba876d2b5f5555a8ca59e03b38c
Romans Kartasovs <kartashow.romans@gmail.com>
2023-10-31 16:29:37 +0000

    env variable for db

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,20 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'a'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATA_BASE_URI')
+
     db.init_app(app)
     from .views import views
     from .auth import auth
